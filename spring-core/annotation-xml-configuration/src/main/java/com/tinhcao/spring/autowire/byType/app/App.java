5ee8fb0a013081d1be572955107dba97ef3b8b65
tinh cao <caodangtinh@gmail.com>
2018-06-18 14:11:44 +0700

    add @Autowire and @Resouse

--- spring-core/annotation-xml-configuration/src/main/java/com/tinhcao/spring/autowire/byType/app/App.java
+++ spring-core/annotation-xml-configuration/src/main/java/com/tinhcao/spring/autowire/byType/app/App.java
@@ -1,14 +1,15 @@
 package com.tinhcao.spring.autowire.byType.app;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.tinhcao.spring.autowire.byType.config.AppConfig;
 import com.tinhcao.spring.autowire.byType.domain.Employee;
 
 public class App {
 
 	public static void main(String[] args) {
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext("autowire-configuration.xml");
+		AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		Employee employee = (Employee) context.getBean("employee");
 		System.out.println(employee.getAddress());
 
