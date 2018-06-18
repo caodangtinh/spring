187f44352165b2a43a2d9c79b653d98bd491b7a4
tinh cao <caodangtinh@gmail.com>
2018-06-18 15:45:23 +0700

    add autowire detect

---
+++ spring-core/spring-auto-detection-autowire-component-scanning/src/main/java/com/tinhcao/spring/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.tinhcao.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.tinhcao.spring")
+public class AppConfig {
+
+}
